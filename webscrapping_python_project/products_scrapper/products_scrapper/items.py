33028f6d74a91bb1d3214594995f417dad8baf76
Mateusz <mpodkans@amazon.pl>
2025-03-14 14:47:55 +0100

    Changing the logic of spiders to use Items class from scrapy and adding ProductItem class for more efficient coding

--- webscrapping_python_project/products_scrapper/products_scrapper/items.py
+++ webscrapping_python_project/products_scrapper/products_scrapper/items.py
@@ -6,7 +6,13 @@
 import scrapy
 
 
-class ProductsScrapperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ProductItem(scrapy.Item):
+    product_title = scrapy.Field()
+    product_url = scrapy.Field()
+    product_price = scrapy.Field()
+    product_description = scrapy.Field()
+    shop = scrapy.Field()
+    scraping_date = scrapy.Field()
+    ean = scrapy.Field()
+    quantity_available = scrapy.Field()
+    image_url = scrapy.Field()
--- webscrapping_python_project/products_scrapper/products_scrapper/spiders/agtom.py
+++ webscrapping_python_project/products_scrapper/products_scrapper/spiders/agtom.py
@@ -1,52 +1,50 @@
 import scrapy
-import datetime  
-
+import datetime
+from products_scrapper.items import ProductItem 
 class AgtomSpider(scrapy.Spider):
     name = "agtom"
     allowed_domains = ["agtom.eu"]
     start_urls = ["https://agtom.eu/51-warhammer-40000"]
 
     custom_settings = { 
-    'ROBOTSTXT_OBEY': True,
-    'DOWNLOAD_DELAY': 2,   
+        'ROBOTSTXT_OBEY': True,
+        'DOWNLOAD_DELAY': 2,   
     }
 
     def parse(self, response):
-            for product in response.css("div.h5 a.product-name"):
-                product_url = product.attrib['href'] 
-                print(product_url) 
-                yield scrapy.Request(
-                    url=product_url ,
-                    callback=self.parse_product,
-                    meta={
-                          'product_url': product_url
-                    }
-                )
-
+        for product in response.css("div.h5 a.product-name"):
+            product_url = product.attrib['href']
+            print(product_url)
+            yield scrapy.Request(
+                url=product_url,
+                callback=self.parse_product,
+                meta={'product_url': product_url}
+            )
 
     def parse_product(self, response):
         title = response.css("h1.product-name::text").get()
-        title = title.replace(","," ")
+        title = title.replace(",", " ") if title else 'No title'
         price = response.css("span#our_price_display::attr(content)").get()
         price = float(price) if price else None
         image_url = response.css('img#bigpic::attr(src)').get()
         ean = response.css("label:contains('EAN') + span::text").get()
         quantity = response.css('span#quantityAvailable::text').get()
         if quantity:
-                quantity = quantity.replace('Dostępna ilość:', '').strip()
+            quantity = quantity.replace('Dostępna ilość:', '').strip()
         description = response.css("p.page-product-heading + div.rte ::text").getall()
         description = " ".join(description).strip()
-        description = description.replace(","," ")
+        description = description.replace(",", " ")
 
+        product_item = ProductItem(
+            product_title=title,
+            product_url=response.meta['product_url'],
+            product_price=price if price else 'No data',
+            product_description=description if description else 'No data',
+            shop='agtom',
+            scraping_date=datetime.date.today().isoformat(),
+            ean=ean.strip() if ean else 'No data',
+            quantity_available=quantity if quantity else 'No data',
+            image_url=response.urljoin(image_url) if image_url else 'No data'
+        )
 
-        yield {
-            'product_title': title.strip() if title else 'No title',
-            'product_url': response.meta['product_url'],
-            'product_price': price if price else 'No data',
-            'product_description': description if description else 'No data',
-            'shop': 'agtom',
-            'scraping_date': datetime.date.today().isoformat(),
-            'ean': ean.strip() if ean else 'No data',
-            'quantity_available': quantity if quantity else 'No data',
-            'image_url': response.urljoin(image_url) if image_url else 'No data'
-        }
+        yield product_item
